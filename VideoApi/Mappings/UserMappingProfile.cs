7f3f8118c683c30095ffd85234576914faa41274
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2025-08-22 21:08:55 +0700

    create: mapping user

---
+++ VideoApi/Mappings/UserMappingProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using VideoApi.Dtos;
+using VideoApi.Models;
+
+namespace VideoApi.Mappings
+{
+    public class UserMappingProfile : Profile
+    {
+        public UserMappingProfile()
+        {
+            CreateMap<RegisterDto, UserModel>();
+        }
+    }
+}
