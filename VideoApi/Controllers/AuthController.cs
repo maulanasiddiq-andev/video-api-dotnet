318721c2c783f0123f9a4ea0185f0af014d57636
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2025-08-22 21:53:07 +0700

    update: register user

--- VideoApi/Controllers/AuthController.cs
+++ VideoApi/Controllers/AuthController.cs
@@ -1,4 +1,11 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using VideoApi.Dtos;
+using VideoApi.Exceptions;
+using VideoApi.Helpers;
+using VideoApi.Models;
+using VideoApi.Repositories;
+using VideoApi.Responses;
 
 namespace VideoApi.Controllers
 {
@@ -6,13 +13,45 @@ namespace VideoApi.Controllers
     [Route("api/[controller]")]
     public class AuthController : ControllerBase
     {
+        private readonly IMapper _mapper;
+        private readonly AuthRepository _authRepository;
+        public AuthController(
+            IMapper mapper,
+            AuthRepository authRepository
+        )
+        {
+            _mapper = mapper;
+            _authRepository = authRepository;
+        }
+
         [HttpPost]
         [Route("register")]
-        public async Task<string> RegisterAsync()
+        public async Task<BaseResponse> RegisterAsync([FromBody] RegisterDto registerDto)
         {
-            await Task.Delay(200);
+            try
+            {
+                var validator = new RegisterValidator();
+                var results = validator.Validate(registerDto);
+                if (!results.IsValid)
+                {
+                    var messages = results.Errors.Select(error => error.ErrorMessage).ToList();
+                    return new BaseResponse(false, messages);
+                }
 
-            return "REGISTER";
+                var user = _mapper.Map<UserModel>(registerDto);
+
+                await _authRepository.RegisterAsync(user, registerDto.Password);
+
+                return new BaseResponse(true, "Pendaftaran Berhasil", null);
+            }
+            catch (KnownException ex)
+            {
+                return new BaseResponse(false, ex.Message, null);
+            }
+            catch (Exception ex)
+            {
+                return new BaseResponse(false, ex.Message, null);
+            }
         }
 
         [HttpPost]
--- VideoApi/Repositories/AuthRepository.cs
+++ VideoApi/Repositories/AuthRepository.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using VideoApi.Constants;
+using VideoApi.Exceptions;
 using VideoApi.Helpers;
 using VideoApi.Models;
 
@@ -15,10 +18,28 @@ namespace VideoApi.Repositories
 
         public async Task RegisterAsync(UserModel user, string password)
         {
+            var existingUser = await FindUserByEmailAsync(user.Email);
+
+            if (existingUser != null)
+            {
+                throw new KnownException($"User dengan email {user.Email} sudah ada");
+            }
+
+            user.UserId = Guid.NewGuid().ToString("N");
             user.HashedPassword = passwordHasherHelper.Hash(password);
+            user.CreatedTime = DateTime.UtcNow;
+            user.ModifiedTime = DateTime.UtcNow;
+            user.RecordStatus = RecordStatusConstant.Active;
 
             await _dBContext.AddAsync(user);
             await _dBContext.SaveChangesAsync();
+        }
+
+        public async Task<UserModel?> FindUserByEmailAsync(string email)
+        {
+            UserModel? user = await _dBContext.User.Where(user => user.Email.ToLower() == email.ToLower()).FirstOrDefaultAsync();
+
+            return user;
         }
     }
 }
