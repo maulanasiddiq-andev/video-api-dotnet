090df7fce9b3248dde1c28e77b6a79b90119981f
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2025-09-03 13:17:50 +0700

    update: add property username in user dto

--- VideoApi/Dtos/UserDto.cs
+++ VideoApi/Dtos/UserDto.cs
@@ -5,6 +5,7 @@ namespace VideoApi.Dtos
         public string UserId { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
         public DateTime? EmailVerifiedTime { get; set; }
         public string? ProfileImage { get; set; }
         public string? CoverImage { get; set; }
