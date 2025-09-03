7634c1b996714ce2c4ebb7f123b6ee9687871157
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2025-09-03 15:59:19 +0700

    update: nullable date time properties

--- VideoApi/Dtos/BaseDto.cs
+++ VideoApi/Dtos/BaseDto.cs
@@ -5,9 +5,9 @@ namespace VideoApi.Dtos
         public uint Version { get; set; }
         public string? Description { get; set; }
         public string? RecordStatus { get; set; }
-        public DateTime CreatedTime { get; set; }
-        public DateTime ModifiedTime { get; set; }
-        public DateTime DeletedTime { get; set; }
+        public DateTime? CreatedTime { get; set; }
+        public DateTime? ModifiedTime { get; set; }
+        public DateTime? DeletedTime { get; set; }
         public string? CreatedBy { get; set; }
         public string? ModifiedBy { get; set; }
         public string? DeletedBy { get; set; }
